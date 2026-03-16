6ddba9a1b5aefc521fccf180b0104eea767a1a7a
SaminoP <samopabis@gmail.com>
2026-03-16 14:56:33 +0100

    finalna verzia

--- README.md
+++ README.md
@@ -5,11 +5,9 @@
 Tento projekt zistí, či sa dve kružnice pretínajú, koľko majú prienikov a vykreslí ich do grafu.
 
 ### Súbory
-- `circle_stats.py` – výpočty (prienik, súčet polomerov, vzdialenosť)
+- `circle_stats.py` – výpočty (prienik, súčet polomerov, vzdialenosť) + testy
 - `circles_intersection_draw.py` – vykreslenie kružníc
 - `circle_intersection.py` – hlavný skript
-- `tests/test_circle_stats.py` – testy funkcií
 
-### Spustenie
-```bash
-python circle_intersection.py
+
+
--- src/circle_project/circle_stats.py
+++ src/circle_project/circle_stats.py
@@ -23,7 +23,7 @@ def ma_prienik(kruz1, kruz2):
 
     # rozhodovanie
     if d > r_suc:
-        
+
         return {"is_intersection": False, "intersections_count": 0}
     elif isclose(d, r_suc) or isclose(d, r_diff):
 
@@ -34,3 +34,38 @@ def ma_prienik(kruz1, kruz2):
     else:
 
         return {"is_intersection": True, "intersections_count": 2}
+
+
+# tesy
+
+from circle_stats import ma_prienik, sucet_polomerov
+
+
+
+# Test 1
+k1 = {"x":0, "y":0, "r":2}
+k2 = {"x":2, "y":0, "r":2}
+print("Test 1:", ma_prienik(k1,k2))
+
+# Test 2
+k1 = {"x":1, "y":1, "r":3}
+k2 = {"x":4, "y":3, "r":2}
+print("Test 2:", ma_prienik(k1,k2))
+
+# Test 3
+k1 = {"x":0, "y":0, "r":2}
+k2 = {"x":4, "y":0, "r":2}
+print("Test 3:", ma_prienik(k1,k2))
+
+# Test 4
+k1 = {"x":0, "y":0, "r":5}
+k2 = {"x":3, "y":0, "r":2}
+print("Test 4:", ma_prienik(k1,k2))
+
+# Test 5
+k1 = {"x":0, "y":0, "r":2}
+k2 = {"x":10, "y":0, "r":3}
+print("Test 5:", ma_prienik(k1,k2))
+
+
+print("Test radius_sum:", sucet_polomerov(3,4))
